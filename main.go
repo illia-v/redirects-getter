ff9544b4f15e827e019dfc74172de6405c481e19
Illia Volochii <illia.volochii@gmail.com>
2018-05-09 23:14:36 +0300

    Implement synchronous getting redirects

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"encoding/json"
 	"redirects_getter/redirects"
+	"fmt"
+	"time"
 )
 
 func main() {
@@ -11,10 +14,26 @@ func main() {
 		"http://docker-host1.cli.bz:8888/test-links.json",
 		"URL of an HTTP resource where links are stored.",
 	)
+	chromeRemoteDebuggingUrl := flag.String(
+		"chrome-remote-debugging-url",
+		"http://localhost:9222",
+		"URL of a Chrome instance for debugging.",
+	)
+	maxTimeToRedirect := flag.Duration(
+		"max-time-to-redirect",
+		5 * time.Second,
+		"Maximum time that you allow the tool to wait for a redirect. " +
+		`Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".`,
+	)
 	flag.Parse()
 
-	_, err := redirects.GetRedirects(*url)
+	r, err := redirects.GetRedirects(*url, *chromeRemoteDebuggingUrl, *maxTimeToRedirect)
 	if err != nil {
 		panic(err)
 	}
+	rJson, err := json.MarshalIndent(r, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(rJson))
 }
--- redirects/redirects.go
+++ redirects/redirects.go
@@ -1,19 +1,36 @@
 package redirects
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/mafredri/cdp"
+	"github.com/mafredri/cdp/devtool"
+	"github.com/mafredri/cdp/protocol/network"
+	"github.com/mafredri/cdp/protocol/page"
+	"github.com/mafredri/cdp/rpcc"
 )
 
-func GetRedirects(url string) (interface{}, error) {
-	_, err := getLinks(url)
+func GetRedirects(url, chromeRemoteDebuggingUrl string, maxTimeToRedirect time.Duration) (map[string][]string, error) {
+	links, err := getLinks(url)
 	if err != nil {
 		return nil, err
 	}
 
-	return nil, nil
+	redirects := make(map[string][]string)
+	for _, link := range links {
+		linkRedirects, err := GetLinkRedirects(link, chromeRemoteDebuggingUrl, maxTimeToRedirect)
+		if err != nil {
+			return nil, err
+		}
+		redirects[link] = linkRedirects
+	}
+
+	return redirects, nil
 }
 
 func getLinks(url string) ([]string, error) {
@@ -36,4 +53,71 @@ func getLinks(url string) ([]string, error) {
 	}
 
 	return links.Links, nil
+}
+
+func GetLinkRedirects(link, chromeRemoteDebuggingUrl string, maxTimeToRedirect time.Duration) ([]string, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Use the DevTools HTTP/JSON API to manage targets (e.g. pages, webworkers).
+	devTools := devtool.New(chromeRemoteDebuggingUrl)
+	pt, err := devTools.Get(ctx, devtool.Page)
+	if err != nil {
+		return nil, err
+	}
+
+	// Initiate a new RPC connection to the Chrome Debugging Protocol target.
+	conn, err := rpcc.DialContext(ctx, pt.WebSocketDebuggerURL)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	c := cdp.NewClient(conn)
+
+	// Open a RequestWillBeSent client to buffer this event.
+	requestWillBeSent, err := c.Network.RequestWillBeSent(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer requestWillBeSent.Close()
+
+	// Enable network events.
+	if err = c.Network.Enable(ctx, network.NewEnableArgs()); err != nil {
+		return nil, err
+	}
+
+	// Navigate to a given page.
+	_, err = c.Page.Navigate(ctx, page.NewNavigateArgs(link))
+	if err != nil {
+		return nil, err
+	}
+
+	// Get redirects.
+	var redirects []string
+	for {
+		redirected := make(chan bool, 1)
+		// Wait for a redirect at most `maxTimeToRedirect`.
+		go func(redirected chan bool) {
+			select {
+			case <-redirected:
+			case <-time.After(maxTimeToRedirect):
+				cancel()
+			}
+		}(redirected)
+
+		// Wait until a `RequestWillBeSent` event is received.
+		requestWillBeSentReply, err := requestWillBeSent.Recv()
+		redirected <- true
+		if ctx.Err() == context.Canceled {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		redirects = append(redirects, requestWillBeSentReply.DocumentURL)
+	}
+
+	return redirects, nil
 }
